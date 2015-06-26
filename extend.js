0855cf84d337f9a8b3f7e43b33f22cebb4f7500c
Kim Schlesinger <schlesinger.kim@gmail.com>
2015-06-26 10:38:40 -0600

    working

--- extend.js
+++ extend.js
@@ -2,20 +2,24 @@ var routes = require('routes')(),
     fs = require('fs'),
     // Configure Monk Driver in the router
     db = require('monk')('localhost/music'),
-    bands = db.get('bands');
+    music = db.get('music');
 
-routes.addRoute('/bands', function(req, res, url) {
+routes.addRoute('/music', function(req, res, url) {
   res.setHeader('Content-Type', 'text/html')
   if (req.method === 'GET') {
     var template = ''
-    bands.find({}, function(err, docs) {
-      console.log(docs);
+    music.find({}, function(err, docs) {
       if (err) res.end('Broken')
       docs.forEach(function(band){
-        template += '<h2>' + band.name + '</h2>'
+        template += '<h2><a href ="/bands' + doc._id + '">' + doc.name + '</h2>'
       })
       res.end(template)
     })
   }
 })
+
+routes.addRoute('/bands/:id', function (req, res, url) {
+  console.log(url.params.id)
+})
+
 module.exports = routes
